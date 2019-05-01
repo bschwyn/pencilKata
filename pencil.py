636cb0702d824074ae8e4d1a07ef94574aec388f
Ben <ben.schwyn@gmail.com>
2019-05-01 13:58:44 -0700

    3 tests pass, on writing, appending, and durability reduction, though one of the tests on durability fails. Another note is that the durability will decrease to either -1 or 0, which I think is an error.

---
+++ pencil.py
@@ -0,0 +1,25 @@
+class Pencil:
+
+    def __init__(self, length, durability, eraser):
+        self.length = length
+        self.durability = durability
+        self.eraser = eraser
+
+    def _decrease_durability(self, char):
+        if char.isupper():
+            self.durability -=2
+        elif char.islower():
+            self.durability -=1
+        else:
+            self.durability -=0
+
+
+    def write(self, text, paper):
+
+        char_list = list(text)
+        paper = list(paper)
+        while char_list:
+            next_char = char_list.pop(0)
+            self._decrease_durability(next_char)
+            paper.append(next_char) if self.durability > 0 else paper.append(' ')
+        return ''.join(paper)
